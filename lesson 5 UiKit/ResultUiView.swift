f91d6eb6d02627d9fc8ac81c1607780d7eaddc48
bekturinho <ismanalievbektur4@gmail.com>
2024-02-04 17:31:09 -0800

    dsdsd

---
+++ lesson 5 UiKit/ResultUiView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultUiView.swift
+//  lesson 5 UiKit
+//
+//  Created by fortune cookie on 2/4/24.
+//
+
+import UIKit
+
+class ResultUiView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
